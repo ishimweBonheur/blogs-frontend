a141b3fd1bca6a5aca0c5b73c5b71648237c8d8b
ishimwe bonheur <ishimerbonheur078@gmail.com>
2025-02-23 06:22:37 -0800

    don well

--- src/components/login&signup/SignupForm.tsx
+++ src/components/login&signup/SignupForm.tsx
@@ -1,9 +1,72 @@
-import React from 'react'
+import { useState } from "react";
+import React from 'react';
+import { useForm } from "react-hook-form";
+interface FormData {
+  Firstname: string;
+  Lastname: string;
+  Email: string;
+  Password: string;
+  Confirmpassword: string;
 
-const SignupForm = () => {
+}
+
+
+function SignupForm() {
+  const [Name, setName] = useState('form');
+  const { register, handleSubmit,formState:{errors} } = useForm<FormData>();
+  console.log(errors);
+  const onSubmit = handleSubmit(({Firstname,Lastname,Email,Password,Confirmpassword}) =>
+    console.log(Firstname,Lastname,Email,Password,Confirmpassword));
   return (
-    <div>
-      Component react
+
+    <div className=' min-h-screen bg-secondary flex flex-col justify-center'>
+      <div className="mx-w-md w-full mx-auto">
+        <div className="text-xl text-center font-medium text-primary"> WELCOME REGISTER
+        </div>
+        <div className=" w-1/2 mx-auto mt-4 bg-white shadow-2xl  p-5  rounded ">
+          <form action="" name="form" className="grid grid-cols-1 sm:grid-cols-2 gap-4" onSubmit={onSubmit}>
+            <div>
+              <label htmlFor="" className="text-sm font-bold text-secondary block mt-1"> Firstname</label>
+              <input type="text" {...register("Firstname", {
+                required: "Firstname is required",
+              })} className="w-full p-2 border border-primary rounded mt-1" />
+             <p className="text-red-500 text-sm">{errors.Firstname?.message}</p>
+            </div>
+            <div>
+              <label htmlFor="" className="text-sm font-bold text-secondary block mt-1"> Lastname</label>
+              <input type="text" {...register("Lastname", {
+                required: "Lastname is required"
+              })} className="w-full p-2 border border-primary rounded mt-1" />
+              <p className="text-red-500 text-sm">{errors.Lastname?.message}</p>
+            </div>
+            <div>
+              <label htmlFor="" className="text-sm font-bold text-secondary block"> Email Address</label>
+              <input type="text" {...register("Email", {
+                required: "email is required"
+              })} className="w-full p-2 border border-primary rounded mt-1" />
+              <p className="text-red-500 text-sm">{errors.Email?.message}</p>
+            </div>
+            <div>
+              <label htmlFor="" className="text-sm font-bold text-secondary block"> Create Password</label>
+              <input type="password"  {...register("Password", {
+                required: "Password is required",
+                minLength: { value: 8, message: "Password must be at least 8 characters" },
+              })} className="w-full p-2 border border-primary rounded mt-1" />
+              <p className="text-red-500 text-sm">{errors.Password?.message}</p>
+            </div>
+            <div className="sm:col-span-2">
+              <label htmlFor="" className="text-sm font-bold text-secondary block"> Confirm password</label>
+              <input type="password"   {...register("Confirmpassword", {
+                required: "Please confirm your password",
+              })} className="w-full p-2 border border-primary rounded mt-1" />
+              {errors.Confirmpassword&&<p className="text-red-500 text-sm">{errors.Confirmpassword.message}</p>}
+            </div>
+
+          </form>
+            <button type='submit' className="w-full py-2 px-4 bg-primary shadow-md  hover:bg-hover hover:shadow-lg rounded-md text-white text-lg text-center mt-7"> Register</button>
+
+        </div>
+      </div>
     </div>
   )
 }
--- src/components/login&signup/index.tsx
+++ src/components/login&signup/index.tsx
@@ -8,7 +8,7 @@ interface FormData {
 
 }
 
-const Login=()=>  {
+const Login = () => {
 
   const [name, setName] = useState('form');
 
@@ -25,14 +25,14 @@ const Login=()=>  {
       <div className=" w-1/3 mx-auto mt-4 bg-white shadow-2xl  p-5  rounded shadow- ">
         <form action="" className="space-y-6" onSubmit={onSubmit}>
           <div>
-            <label htmlFor="" className="text-sm font-bold text-secondary block"> email</label>
+            <label htmlFor="" className="text-sm font-bold text-secondary block"> Email</label>
             <input {...register("email", { required: "email is required" })} type="text" className="w-full p-2 border border-primary rounded mt-1 " />
             {errors.email && (
               <p className="text-red-500 text-sm">{errors.email.message}</p>
             )}
           </div>
           <div>
-            <label htmlFor="" className="text-sm font-bold text-secondary block"> password</label>
+            <label htmlFor="" className="text-sm font-bold text-secondary block"> Password</label>
             <input {...register("password", { required: "Password is required", minLength: { value: 8, message: "Password must be at least 8 characters" } })} type="password" className="w-full p-2 border border-primary rounded mt-1" />
             {errors.password && <p className="text-red-500 text-sm">{errors.password.message}</p>}
 
@@ -40,13 +40,13 @@ const Login=()=>  {
           <div className="flex items-center justify-between">
             <div className="flex items-center">
               <input {...register("remember")} type="checkbox" className="h-4 w-4 text-blue-300 rounded" />
-              <label htmlFor="" className="ml-2 text-sm text-primary"> remember me</label>
+              <label htmlFor="" className="ml-2 text-sm text-primary"> Remember me</label>
             </div>
             <div>
-              <a href="" className="font-medium text-sm text-primary">forgot password?</a>
+              <a href="" className="font-medium text-sm text-primary">Forgot password?</a>
             </div>
           </div>
-          <button type='submit' className="w-full py-2 px-4 bg-primary shadow-md  hover:bg-hover hover:shadow-lg rounded-md text-white text-sm text-center"> login</button>
+          <button type='submit' className="w-full py-2 px-4 bg-primary shadow-md  hover:bg-hover hover:shadow-lg rounded-md text-white text-sm text-center"> Login</button>
         </form>
 
       </div>
