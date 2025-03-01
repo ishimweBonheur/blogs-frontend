233514a1c2d95ecd0473663e291f563f0fae1275
ishimwe bonheur <ishimerbonheur078@gmail.com>
2025-03-01 12:44:40 -0800

    night

--- src/components/Header/Sidebar.tsx
+++ src/components/Header/Sidebar.tsx
@@ -1,8 +1,15 @@
 import React from 'react'
+interface SideProps{
+  isSidebarOpen:boolean;
+  toggleSidebar: () => void;
+}
 
-function Sidebar() {
+function Sidebar({isSidebarOpen,toggleSidebar}:SideProps) {
   return (
-    <div>sidebar section</div>
+    <aside className={`fixed top-0 left-0 z-40 w-64 h-screen pt-20 bg-primary border-r shadow-sm sm:translate-x-0 dark:bg-primary dark:border-hover transition-transform ${isSidebarOpen?"translate-x-0":"-translate-x-full"}`}>
+   
+  sidebar
+    </aside>
   )
 }
 
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,7 +3,7 @@ import { Navigate, Route, Routes, useLocation } from 'react-router-dom';
 import Login from '../components/login&signup';
 import SignupForm from '../components/login&signup/SignupForm';
 import Header from '../components/Header/Header';
-import { useState } from 'react';
+import { useState,useEffect } from 'react';
 import Sidebar from '../components/Header/Sidebar';
 
 function AppRoutes() {
@@ -15,13 +15,21 @@ function AppRoutes() {
     setDarkMode(!darkMode);
   };
   const toggleSidebar = () => {
-    setIsSidebarOpen(!isSidebarOpen);
+    setIsSidebarOpen((prev)=>!prev);
   };
   const hideHeaderAndSidebar = location.pathname === "/login" || location.pathname === "/register";
+  useEffect(() => {
+    if (isSidebarOpen) {
+      document.body.classList.add("overflow-hidden");
+    } else {
+      document.body.classList.remove("overflow-hidden");
+    }
+  }, [isSidebarOpen]);
   return (
     <div className={darkMode ? "dark" : ""}>
 
-      {!hideHeaderAndSidebar && <Header darkMode={darkMode} toggleDarkMode={toggleDarkMode} />}
+      {!hideHeaderAndSidebar && <Header darkMode={darkMode} toggleDarkMode={toggleDarkMode} toggleSidebar={toggleSidebar} />    }
+      {!hideHeaderAndSidebar&& <Sidebar isSidebarOpen={isSidebarOpen} toggleSidebar={toggleSidebar}/>}
       <Routes>
         <Route path="/" element={<Navigate to="/login" />} />
         <Route path="/login" element={<Login />} />
