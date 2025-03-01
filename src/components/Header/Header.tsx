f37ab96885d2bf304add299432e8e8662cfb2af2
ishimwe bonheur <ishimerbonheur078@gmail.com>
2025-03-01 08:07:11 -0800

    shard

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,18 +1,27 @@
 import {FaMoon,FaSun} from "react-icons/fa";
 import{HiOutlineMenuAlt2} from "react-icons/hi";
 import{MdSpaceDashboard } from "react-icons/md"; 
+import { Link } from "react-router-dom";
 import React from 'react'
+interface HeaderProps {
+  darkMode: boolean;
+  toggleDarkMode: () => void;
+}
 
-function Header() {
+function Header({darkMode ,toggleDarkMode}:HeaderProps) {
   return (
     <nav className='fixed top-0 z-50 w-full bg-white border-b border-gray-200 shadow-sm'>
       <div className="px-3 py-3 lg:px-5 lg:pl-3">
-        <div className="flex items center justify-center">
+        <div className="flex items-center justify-between">
           <div className="flex items-center justify-start rtl:justify-end ">
             <button>
-              <HiOutlineMenuAlt2/>
+              <HiOutlineMenuAlt2 className="text-2xl"/>
             </button>
+            <Link to="#">
+            <MdSpaceDashboard className="h-9 me-3  text-xl text-black"/>
+            </Link>
           </div>
+          <button onClick={toggleDarkMode}>{darkMode?<FaSun/>:<FaMoon/>}</button>
         </div>
       </div>
     </nav>
