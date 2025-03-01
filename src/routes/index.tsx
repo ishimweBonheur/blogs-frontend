8d5f5726affff3b85a533d32d7302b7e709c7cfa
ishimwe bonheur <ishimerbonheur078@gmail.com>
2025-03-01 09:07:08 -0800

    share

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,14 +3,20 @@ import { Navigate, Route, Routes } from 'react-router-dom';
 import Login from '../components/login&signup';
 import SignupForm from '../components/login&signup/SignupForm';
 import Header from '../components/Header/Header';
+import { useState } from 'react';
 
 function AppRoutes() {
+  const [darkMode, setDarkMode] = useState(false);
+
+  const toggleDarkMode = () => {
+    setDarkMode(prev => !prev);
+  };
   return (
     <Routes>
       <Route path="/" element={<Navigate to="/login" />} />
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<SignupForm />} />
-      <Route path="/header" element={<Header />} />
+      <Route path="/header" element={<Header darkMode={darkMode} toggleDarkMode={toggleDarkMode} />} />
     </Routes>
   );
 }
