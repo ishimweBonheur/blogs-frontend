7d84b524383073eb782d68f18ac5e1bd60491800
ishimwe bonheur <ishimerbonheur078@gmail.com>
2025-03-04 23:38:33 +0200

    changes evening

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -4,7 +4,7 @@ import { FaUserGraduate, FaChalkboardTeacher, FaMoneyBillWave } from 'react-icon
 function Home() {
   return (
     <div className="">
-<div className="grid grid-cols-1 md:grid-cols-3 gap-5 px-8 mt-10 ">
+<div className="grid grid-cols-1 sm:grid-cols-3 gap-5 px-8 mt-10 ">
   <div className="w-full md:w-[400px] bg-background dark:bg-mini dark:text-background  rounded-lg h-[120px] flex flex-col items-center justify-center text-xl font-semibold p-5 shadow-lg hover:shadow-xl transition-all duration-300">
     <FaUserGraduate className="text-blue-500 text-3xl mb-3" />
     <span>Total Students</span>
