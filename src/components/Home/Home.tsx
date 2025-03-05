0b17f7786761c1d4f9214511837ad869f1ca331c
ishimwe bonheur <ishimerbonheur078@gmail.com>
2025-03-05 21:26:59 +0200

    afternoon

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -10,7 +10,7 @@ const Home = () => {
     labels: ['Boys', 'Girls', 'Drops'],
     datasets: [
       {
-        data: [55, 35, 10],
+        data: [400, 400, 200],
         backgroundColor: ['#001d3d', '#659AC6', '#0F22A2'],
         borderColor: ['#001d3d', '#659AC6', '#0F22A2'],
         borderWidth: 1,
@@ -37,19 +37,28 @@ const Home = () => {
   return (
     <div className="w-full">
       <div className="grid grid-cols-1 sm:grid-cols-3 gap-5 px-8 mt-5">
-        <div className="w-full md:w-[400px] bg-background dark:bg-mini dark:text-background rounded-lg h-[100px] flex flex-col items-center justify-center text-xl font-semibold p-5 shadow-lg hover:shadow-xl transition-all duration-300">
-          <FaUserGraduate className="text-blue-500 text-3xl mb-3" />
-          <span>Total Students</span>
+        <div className="w-full md:w-[400px] bg-background dark:bg-mini dark:text-background rounded-lg h-[100px] flex flex-row items-center justify-start  text-xl font-semibold p-5 shadow-lg hover:shadow-xl transition-all duration-300 gap-10">
+          <FaUserGraduate className="text-blue-500 text-3xl ml-5" />
+           <div className='flex flex-col'>
+         <span className="text-md font-bold">2000</span>
+          <span className="text-sm font-semibold text-hover dark:text-background">Total Students</span>
+          </div>
         </div>
 
-        <div className="w-full md:w-[400px] bg-background dark:bg-mini dark:text-background rounded-lg h-[100px] flex flex-col items-center justify-center text-xl font-semibold p-5 shadow-lg hover:shadow-xl transition-all duration-300">
+        <div className=" w-full md:w-[400px] bg-background dark:bg-mini dark:text-background rounded-lg h-[100px] flex flex-row items-center justify-start text-xl font-semibold p-5 shadow-lg hover:shadow-xl transition-all duration-300 gap-10">
           <FaChalkboardTeacher className="text-green-500 text-3xl mb-3" />
-          <span>Teachers</span>
+          <div className="flex flex-col">
+          <span className="text-md font-bold">120</span>
+          <span className="text-sm font-semibold text-hover dark:text-background">Teachers</span>
+          </div>
         </div>
 
-        <div className="w-full md:w-[400px] bg-background dark:bg-mini dark:text-background rounded-lg h-[100px] flex flex-col items-center justify-center text-xl font-semibold p-5 shadow-lg hover:shadow-xl transition-all duration-300">
+        <div className="w-full md:w-[400px] bg-background dark:bg-mini dark:text-background rounded-lg h-[100px] flex flex-row items-center justify-start text-xl font-semibold p-5 shadow-lg hover:shadow-xl transition-all duration-300 gap-10">
           <FaMoneyBillWave className="text-yellow-500 text-3xl mb-3" />
-          <span>Yearly Budget</span>
+          <div className="flex flex-col">
+          <span className="text-md font-bold">$1000</span>
+          <span className="text-sm font-semibold text-hover dark:text-background">Yearly Budget</span>
+          </div>
         </div>
       </div>
 
